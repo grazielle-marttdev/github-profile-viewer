f6dd2c0c30d7764d57c4066ea7235c0e2790ac02
Grazielle <gzassuncaomartins@gmail.com>
2025-11-04 14:04:04 -0300

    Adiciona verificação caso não tenha um nome

--- src/js/profileView.js
+++ src/js/profileView.js
@@ -18,7 +18,7 @@ export function renderProfile(userData, userRepos, container) {
     <div class="profile-card">
         <img src="${userData.avatar_url}" alt="Avatar de ${userData.name}" class="profile-avatar">
         <div class="profile-info">
-        <h2>${userData.name}</h2>
+        <h2>${userData.name || "Não possui nome cadastrado 😢."}</h2>
         <p>${userData.bio || "Não possui bio cadastrada 😢."}</p>
         </div>
     </div>
